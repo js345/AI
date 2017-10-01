36599d52b6cba6cc4e119538dd3c2e8a9d21c3ea
js345 <xyu37@illinois.edu>
2017-10-01 17:26:19 -0500

    1.2

---
+++ mp1/1.2/maze.py
@@ -0,0 +1,73 @@
+
+from queue import Queue, LifoQueue, PriorityQueue
+from search import Search
+from state import State
+
+
+class Maze:
+	"""
+	Maze class to store input maze, start position and dot positions
+	"""
+
+	def __init__(self, filename):
+		# maze is a list of list of chars
+		self.graph = list()
+		# dots positions is a list of tuples of (i, j)
+		self.dots = list()
+		# start point
+		self.start = (0, 0)
+		# dimension of maze
+		self.n = 0
+		self.m = 0
+
+		# read the maze graph from file
+		with open(filename, "r") as file:
+			for i, line in enumerate(file.readlines()):
+				self.graph.append(list(line))
+				for j, c in enumerate(list(line)):
+					if c == 'P':
+						self.start = (i, j)
+					elif c == '.':
+						self.dots.append((i, j))
+
+		# update dimension of the maze
+		self.n = len(self.graph)
+		self.m = len(self.graph[0]) if self.n > 0 else 0
+
+	def display_maze(self):
+		"""
+		Display function to show the current maze status
+		:return: None
+		:rtype: None
+		"""
+		for line in self.graph:
+			print(''.join(line))
+
+
+def build_search(name, maze):
+	# create the start state
+	x, y = maze.start
+	bits = tuple(1 for _ in range(len(maze.dots)))
+	start_state = State(x, y, bits)
+	# enumerate the goal states
+	goal_states = set()
+	end = tuple(0 for _ in range(len(maze.dots)))
+	for x, y in maze.dots:
+		goal_states.add(State(x, y, end))
+	# construct the search object
+	print("============Working on Maze %s ===============\n" % name)
+	search = Search(start_state, goal_states, maze)
+	# print("====Running BFS======")
+	# frontier = Queue()
+	# print(search.bfs(frontier))
+
+	print("====Running A*======")
+	frontier = PriorityQueue()
+	print(search.astar(frontier))
+
+if __name__ == "__main__":
+	path = "input/"
+	maze_files = ["tinySearch.txt", "smallSearch.txt", "mediumSearch.txt"]
+	for maze_file in maze_files:
+		maze = Maze(path+maze_file)
+		build_search(maze_file, maze)
--- mp1/1.2/search.py
+++ mp1/1.2/search.py
@@ -38,9 +38,10 @@ class Search:
 		self.path_cost = 0
 		self.node_number = 0
 
-	def manhattan(self, state):
+	@staticmethod
+	def manhattan(state, dot):
 		x, y = state.x, state.y
-		return abs(self.maze.dots[0][0] - x) + abs(self.maze.dots[0][1] - y)
+		return abs(dot[0] - x) + abs(dot[1] - y)
 
 	def display(self, state):
 		"""
@@ -56,9 +57,17 @@ class Search:
 			state = state.parent
 		states.reverse()
 		graph = deepcopy(self.maze.graph)
+		dot = '0'
 		for state in states:
 			x, y = state.x, state.y
-			graph[x][y] = '.'
+			if state.capture:
+				graph[x][y] = dot
+				if dot == '9':
+					dot = 'a'
+				elif dot == 'z':
+					dot = 'A'
+				else:
+					dot = chr(ord(dot) + 1)
 		res = ""
 		for line in graph:
 			res += ''.join(line)
@@ -97,45 +106,6 @@ class Search:
 					frontier.put(expand)
 					self.visited.add(expand)
 
-	def dfs(self, frontier):
-		self.reset()
-		return self.bfs(frontier)
-
-	def greedy(self, frontier):
-		self.reset()
-		frontier.put((0, self.start_state))
-		self.visited.add(self.start_state)
-		# start search, unreachable if return None
-		while not frontier.empty():
-			h, state = frontier.get()
-			self.node_number += 1
-			if state in self.goal_states:
-				print("Expanded %r nodes" % self.node_number)
-				print("The path cost is %r" % state.cost)
-				return self.display(state)
-			for di in Search.dirs:
-				x, y = state.x + di[0], state.y + di[1]
-				# not a wall
-				if x < 0 or y < 0 or x >= self.maze.n or y >= self.maze.m or self.maze.graph[x][y] == '%':
-					continue
-				# deepcopy bits from the current state
-				next_bits = list(deepcopy(state.bits))
-				# if it's a dot and not eaten yet, change the bit to zero
-				capture = False
-				if self.maze.graph[x][y] == '.' and state.bits[self.maze.dots.index((x, y))] == 1:
-					capture = True
-					next_bits[self.maze.dots.index((x, y))] = 0
-				# convert back to tuple so we can hash it
-				next_bits = tuple(next_bits)
-				expand = State(x, y, next_bits, capture)
-				expand.cost = state.cost + 1
-				# compute heuristic for manhattan distance
-				expand.heuristic = self.manhattan(expand)
-				expand.parent = state
-				if expand not in self.visited:
-					frontier.put((expand.heuristic, expand))
-					self.visited.add(expand)
-
 	def astar(self, frontier):
 		self.reset()
 		frontier.put((0, self.start_state))
@@ -164,9 +134,41 @@ class Search:
 				next_bits = tuple(next_bits)
 				expand = State(x, y, next_bits, capture)
 				expand.cost = state.cost + 1
-				# compute heuristic for manhattan distance and current cost
-				expand.heuristic = self.manhattan(expand) + expand.cost
+				# compute heuristic and add current cost
+				expand.heuristic = self.nearest_heuristic(expand) + expand.cost
 				expand.parent = state
 				if expand not in self.visited:
 					frontier.put((expand.heuristic, expand))
 					self.visited.add(expand)
+
+	def sum_heuristic(self, state):
+		"""
+		Heuristic of adding up all manhattan distance from current state to all
+		remaining dots not collected.
+		:param state: 
+		:type state: 
+		:return: 
+		:rtype: 
+		"""
+		dots = self.maze.dots
+		heuristic = 0
+		for idx, dot in enumerate(dots):
+			if state.bits[idx] == 1:
+				heuristic += Search.manhattan(state, dot)
+		return heuristic
+
+	def nearest_heuristic(self, state):
+		"""
+		Heuristic of finding manhattan distance from current state to the
+		closest dot not collected.
+		:param state: 
+		:type state: 
+		:return: 
+		:rtype: 
+		"""
+		dots = self.maze.dots
+		heuristic = 9999999999
+		for idx, dot in enumerate(dots):
+			if state.bits[idx] == 1 and heuristic >= Search.manhattan(state, dot):
+				heuristic = Search.manhattan(state, dot)
+		return heuristic
