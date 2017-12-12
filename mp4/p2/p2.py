bf28b8036a4dd96f9782d51efbdadb3738731e3b
js345 <xyu37@illinois.edu>
2017-12-11 20:14:53 -0600

    Recommended states

--- mp4/p2/p2.py
+++ mp4/p2/p2.py
@@ -1,10 +1,12 @@
 
-import math, random
+import math
+import random
 
 
 class State:
 
 	paddle_height = 0.2
+	grid_size = 13
 
 	def __init__(self, ball_x, ball_y, velocity_x, velocity_y, paddle_y):
 		self.ball_x = ball_x
@@ -18,8 +20,8 @@ class State:
 		vy = '+' if self.velocity_y > 0 else '-'
 		if abs(self.velocity_y) < 0.015:
 			vy = '0'
-		paddle = str(int(math.floor(self.paddle_y*12/(1-State.paddle_height))))
-		return ','.join([str(int(self.ball_x*12)), str(int(self.ball_y*12)), vx, vy, paddle])
+		paddle = str(int(math.floor(self.paddle_y*State.grid_size/(1-State.paddle_height))))
+		return ','.join([str(int(self.ball_x*State.grid_size)), str(int(self.ball_y*State.grid_size)), vx, vy, paddle])
 
 	def update_ball(self):
 		"""
@@ -56,7 +58,7 @@ class State:
 		self.paddle_y = min(max(0.0, self.paddle_y), 1-State.paddle_height)
 
 	@staticmethod
-	def update_reward(state, reward, max_reward, alpha=1, c=65, gamma=0.88):
+	def update_reward(state, reward, max_reward, alpha=1, c=100, gamma=0.9):
 		"""
 		Update state rewards and action counts of states
 		:param state: state in string representation
@@ -79,16 +81,7 @@ class State:
 		rewards[state] = rewards.get(state, 0.0) + alpha*(reward+gamma*max_reward-rewards.get(state, 0.0))
 
 	@staticmethod
-	def find_max_reward(state):
-		return max([rewards.get(state + str(m), 0.0) for m in range(3)])
-
-	@staticmethod
-	def find_best_move(state, greedy=False):
-		nums = [rewards.get(state+str(m), 0.0) if greedy else State.explore(rewards.get(state+str(m), 0.0), actions.get(state+str(m), 0.0)) for m in range(3)]
-		return random.choice([idx for idx, r in enumerate(nums) if r == max(nums)])
-
-	@staticmethod
-	def explore(reward, count, e=70):
+	def explore(reward, count, e=90):
 		if count >= e:
 			return reward
 		return 1001
@@ -98,27 +91,24 @@ if __name__ == "__main__":
 	moves = [-0.04, 0, 0.04]
 	rewards, actions = dict(), dict()
 	score = 0
-	alpha = 1
 	for it in range(100000):
 		s = State(0.5, 0.5, 0.03, 0.01, 0.5 - State.paddle_height/2)
 		prev_s, prev_r = None, 0
 		# training
-		while True:
-			if prev_r < 0:
-				State.update_reward(prev_s, prev_r, -1, alpha=alpha)
-				score += 1
-				break
+		while prev_r >= 0:
 			curr_s = s.__str__()
 			curr_r = s.update_ball()
 			if prev_s is not None:
-				State.update_reward(prev_s, prev_r, State.find_max_reward(curr_s), alpha=alpha)
-			action = State.find_best_move(curr_s)
+				State.update_reward(prev_s, prev_r, max([rewards.get(curr_s + str(m), 0.0) for m in range(3)]))
+			nums = [State.explore(rewards.get(curr_s + str(m), 0.0), actions.get(curr_s + str(m), 0.0)) for m in range(3)]
+			action = random.choice([idx for idx, r in enumerate(nums) if r == max(nums)])
 			prev_s = curr_s + str(action)
 			s.update_paddle(moves[action])
 			prev_r = curr_r
-			score += curr_r
+			score += max(curr_r, 0)
+		State.update_reward(prev_s, prev_r, -1)
 		if it % 1000 == 999:
-			print(str(score/1000) + " at round " + str(it+1))
+			print(str(score/1000) + " at round " + str(int(it/1000)))
 			score = 0
 	print(len(actions))
 	print(sum(1 for action in actions if actions[action] < 65))
